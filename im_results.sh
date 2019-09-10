878f5c48c7adad270f90750bdff5452705a31f19
Milan Dufek <37630858+milandufek@users.noreply.github.com>
2019-09-10 09:38:58 +0200

    Update im_results.sh

--- im_results.sh
+++ im_results.sh
@@ -9,6 +9,7 @@ RES_FILE=$(echo $BASE_LINK | awk -F '/' '{print $NF}') && rm -f ${RES_FILE}*
 wget $BASE_LINK || exit $?
 PAGES=$(grep -Eo "results.aspx\?p=[0-9]{2,3}" $RES_FILE | tail -1 | awk -F '=' '{print $NF}')
 
+echo "Downloading result pages..."
 for i in $(seq 2 $PAGES); do
     wget -q ${BASE_LINK}?p=$i
 done
