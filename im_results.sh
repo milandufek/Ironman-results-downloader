72338bc72897e716ba57a64973c9f79d29f97c6d
Milan Dufek <dufek.milan@gmail.com>
2019-11-07 20:12:31 +0100

    Playing

--- im_results.sh
+++ im_results.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # e.g.
-# BASE_LINK="https://www.ironman.com/triathlon/events/emea/ironman-70.3/zell-am-see-kaprun/results.aspx"
 # BASE_LINK="https://eu.ironman.com/triathlon/events/emea/ironman/austria/results.aspx"
 
 BASE_LINK="https://eu.ironman.com/triathlon/events/emea/ironman/austria/results.aspx"
@@ -21,7 +20,7 @@ fi
 cd $TMP_DIR
 RES_FILE=$(echo $BASE_LINK | awk -F '/' '{print $NF}') && rm -f ${RES_FILE}*
 
-echo "Downloader started"
+echo "Results download started"
 wget -q $BASE_LINK || download_failed $?
 PAGES=$(grep -Eo "results.aspx\?p=[0-9]{2,3}" $RES_FILE | tail -1 | awk -F '=' '{print $NF}')
 
@@ -31,9 +30,10 @@ BAR=$PROGRESS_CHAR
 for PAGE in $(seq 2 $PAGES); do
     [ $(($PAGE % 10)) -eq 0 ] && BAR="${BAR}${PROGRESS_CHAR}"
     [ $PAGE -eq $PAGES ] && STATUS_MSG="Completed"
-    printf "%-12s %-40s (%d/%d)\r" "$STATUS_MSG" $BAR $PAGE $PAGES
+    printf "%-12s %-32s (%d/%d)\r" "$STATUS_MSG" $BAR $PAGE $PAGES
     wget -q ${BASE_LINK}?p=$PAGE || download_failed $?
-done && echo -e "\n"
+done
+echo -e "\n"
 
 echo "Surname;Name;Country;DivRank;GenderRank;OverallRank;Swim;Bike;Run;Finish" > $RESULTS
 grep ";bidid=" ${RES_FILE}* \
@@ -44,6 +44,8 @@ grep ";bidid=" ${RES_FILE}* \
          -e 's/<\/td>//g' \
    | tr -d ' ' >>$RESULTS
 
+[ -f $RESULTS ] && echo "Find results in $RESULTS"
 rm -f ${RES_FILE}*
 cd $START_DIR
+
 exit 0
